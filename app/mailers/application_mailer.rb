7379f66b4834328275485e11f67e79c50a46664e
yuzu5 <aoki08160816@gmail.com>
2019-04-11 01:58:56 +0900

    アカウント有効化

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@example.com', charset: 'ISO-2022-JP'
+  default from: 'noreply@example.com'
   layout 'mailer'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
-  before_save :downcase_email
+  before_save   :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -61,7 +61,7 @@ private
 
   # 有効化トークンとダイジェストを作成および代入する
   def create_activation_digest
-    self.activation_token = User.new_token
+    self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
 
