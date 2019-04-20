d9083d23a685ca9e34d243c661895e60667f418c
yuzu5 <aoki08160816@gmail.com>
2019-04-20 15:16:29 +0900

    マイクロポスト機能を追加

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,7 +6,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    storage :fog
+    #storage :fog
+    storage :file
   else
     storage :file
   end
